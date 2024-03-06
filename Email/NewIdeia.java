38708f987f5a4233abbbfa1fd8610b69d56d0ee5
Pedro <1231852@isep.ipp.pt>
2024-03-06 11:41:01 +0000

    repetir sout

--- Email/NewIdeia.java
+++ Email/NewIdeia.java
@@ -4,6 +4,11 @@ public class NewIdeia {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 
 
