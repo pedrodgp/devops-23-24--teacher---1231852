089e4ed2b29c0f78eeef38301eaa9b5647f76489
Pedro <1231852@isep.ipp.pt>
2024-03-06 11:37:48 +0000

    new ideia

--- Email/field.java
+++ Email/field.java
@@ -5,9 +5,16 @@ public class field {
     String name;
     String value;
 
-
-    public field(String name, String value){
+    int id;
+//new ideia
+//
+//
+//
+//
+//
+public field(String name, String value){
         this.name = name;
         this.value = value;
+        this.id = 0;
     }
 }
