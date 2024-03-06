eb0fb80f168a0dedb80a6106f735d10bbd23ed1b
Pedro <1231852@isep.ipp.pt>
2024-03-06 11:14:17 +0000

    tentativa de branch

---
+++ Email/field.java
@@ -0,0 +1,13 @@
+package Email;
+
+public class field {
+
+    String name;
+    String value;
+
+
+    public field(String name, String value){
+        this.name = name;
+        this.value = value;
+    }
+}
