9c75fdb6c32f71cc313310f660f228aa1b99f78b
Pedro <1231852@isep.ipp.pt>
2024-03-07 00:35:07 +0000

    Email

--- Email/NewIdeia.java
+++ Email/NewIdeia.java
@@ -3,12 +3,7 @@ package Email;
 public class NewIdeia {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
+
     }
 
 
