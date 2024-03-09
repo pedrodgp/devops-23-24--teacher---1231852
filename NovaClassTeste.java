6c403be1f804ce6711189b77b1fd7778dd7af92a
Pedro <1231852@isep.ipp.pt>
2024-03-09 10:14:26 +0000

    commit, #8

--- NovaClassTeste.java
+++ NovaClassTeste.java
@@ -1,9 +1,4 @@
 public class NovaClassTeste {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-
-        System.out.println("Bye, World!");
-
-        System.out.println("Bug, World!");
     }
 }
