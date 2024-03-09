efac95374b66572f17c73e59b3dd15568b4f84c8
Pedro <1231852@isep.ipp.pt>
2024-03-09 10:10:26 +0000

    trabalhando no algo, issue #4

--- Email/field.java
+++ Email/field.java
@@ -2,19 +2,5 @@ package Email;
 
 public class field {
 
-    String name;
-    String value;
 
-    int id;
-//new ideia
-//
-//
-//
-//
-//
-public field(String name, String value){
-        this.name = name;
-        this.value = value;
-        this.id = 0;
-    }
 }
