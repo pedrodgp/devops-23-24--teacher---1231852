796dea161a28c78eba2e116d9f8637bb571ea820
Pedro <1231852@isep.ipp.pt>
2024-03-09 10:11:42 +0000

    trabalhando no algo mais, issue #7

--- NovaClassTeste.java
+++ NovaClassTeste.java
@@ -1,13 +1,7 @@
 public class NovaClassTeste {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
 
-        System.out.println("Bye, World!");
-        System.out.println("Bye, World!");
-        System.out.println("Bye, World!");
         System.out.println("Bye, World!");
 
         System.out.println("Bug, World!");
